2abede08d3c21ae2e8c6082a6ef0e69a302f78d9
Tu Dang <tudang87@gmail.com>
2015-01-04 19:59:11 +0100

    more stuff comes to plots

--- plot_bwn_data.r
+++ plot_bwn_data.r
@@ -13,12 +13,14 @@ for (i in args) {
 	'pkt_outs', 'pkt_ins', 'pkts', 'pkt_in', 'pkt_out', 
 	'err_outs', 'err_ins', 'err_in', 'err_out'))
 	
-	t = subset(d, itf=='enp1s0', select=c(tmp,b_outs,b_ins,bs,pkts))
+	t = subset(d, itf=='enp1s0', select=c(tmp,b_outs,b_ins,bs,pkt_ins, pkt_outs, pkts))
 
 	name <- strsplit(i, "[.]")
 	name <- name[[1]][1]
 	t$role <- name
 	t$Mbps <- t$bs*8 / 2^20
+	t$b_outs <- t$b_outs*8 / 2^20
+	t$b_ins <- t$b_ins*8 / 2^20
 	st <- t$tmp[1]
 	t$tmp <- t$tmp - st
 	data <- rbind(data, t)
@@ -26,12 +28,40 @@ for (i in args) {
 
 
 pdf("rplot.pdf")
+
+ggplot(data, aes(x=tmp, y=b_outs, color=role)) + 
+coord_cartesian() +
+scale_y_continuous() + 
+geom_line() + 
+expand_limits(y=0) + 
+xlab("Time") + ggtitle("Number of bytes out")
+
+ggplot(data, aes(x=tmp, y=b_ins, color=role)) + 
+coord_cartesian() +
+scale_y_continuous() + 
+geom_line() + 
+expand_limits(y=0) + 
+xlab("Time") + ggtitle("Number of bytes in")
+
+
 ggplot(data, aes(x=tmp, y=Mbps, color=role)) + 
 coord_cartesian() +
 scale_y_continuous() + 
 geom_line() + 
 expand_limits(y=0) + 
-xlab("Time") + ggtitle("Network Throughput")
+xlab("Time") + ggtitle("Total bytes")
+
+ggplot(data, aes(x=tmp, y=pkt_ins, color=role)) + 
+coord_cartesian() +
+scale_y_continuous() + 
+geom_line() + xlab("Time") + 
+ggtitle("Number of Packets in")
+
+ggplot(data, aes(x=tmp, y=pkt_outs, color=role)) + 
+coord_cartesian() +
+scale_y_continuous() + 
+geom_line() + xlab("Time") + 
+ggtitle("Number of Packets out")
 
 ggplot(data, aes(x=tmp, y=pkts, color=role)) + 
 coord_cartesian() +
