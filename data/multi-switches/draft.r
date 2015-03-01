94433beb413d8d502a1501b7ac36e8650e14dce0
Tu Dang <tudang87@gmail.com>
2015-03-01 11:35:48 +0100

    add jitter when datapoint overlap

--- data/multi-switches/draft.r
+++
@@ -1,9 +0,0 @@
-library('ggplot2')
-
-args<-commandArgs(TRUE)
-
-pdf('plot.pdf')
-s = read.csv(args[1], col.names=c('interface', 'throughput', 'lost'))
-ggplot(s, aes(x=throughput, y=lost, colour=interface)) + geom_point(aes(group=interface), size=3) 
-dev.off()
-
---
+++ data/multi-switches/plot-single.r
@@ -0,0 +1,11 @@
+library('ggplot2')
+
+args<-commandArgs(TRUE)
+
+pdf('plot.pdf')
+s = read.csv(args[1], col.names=c('interface', 'throughput', 'lost'))
+ggplot(s, aes(x=throughput, y=lost, colour=interface)) + 
+geom_point(aes(group=interface), size=3, 
+                position=position_jitter(width=5,height=0)) 
+dev.off()
+
