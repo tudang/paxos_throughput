ad07d0e728a1d2f8c07685ff0b2acca9523a853c
Tu Dang <tudang87@gmail.com>
2015-03-07 21:35:00 +0100

    plot all figures

--- SOSR15/figures/plot-figures.R
+++ SOSR15/figures/plot-figures.R
@@ -1,3 +1,6 @@
+library("bear")
+library("Hmisc")
+
 plot_broadcast <- function() {
     dfc = read.csv('csv/input.csv', header=F, col.names=c('proto', 'tput', 'latency'))
     tputs <- dfc$tput
@@ -13,7 +16,7 @@ plot_broadcast <- function() {
     pdf("figures/broadcast-latency.pdf")
     op <- par(mar = c(4, 8, 4, 2) + 0.1)
     barplot(latencies, ylim=c(0, 2.1), cex.lab=2.5, cex.axis=2.0, cex.names=2.0)
-    title(ylab = "Latency (us)", cex.lab = 2.5, line = 4.5)
+    title(ylab = "Latency (ms)", cex.lab = 2.5, line = 4.5)
     box()
     dev.off()
 }
@@ -42,12 +45,62 @@ plot_netpaxos_tput <- function() {
     pdf("figures/netpaxos-latency.pdf")
     op <- par(mar = c(4, 8, 4, 2) + 0.1)
     barplot(latency, ylim=c(0,2.1), cex.lab=2.5, cex.axis=2.0, cex.names=2.0)
-    title(ylab = "Latency (us)", cex.lab = 2.5, line = 4.5)
+    title(ylab = "Latency (ms)", cex.lab = 2.5, line = 4.5)
     box()
 dev.off()
+}
+
+plot_netpaxos_latency <- function() {
+    df <- read.csv(file="csv/netpaxos-tput-latency.csv", col.names=c('host','interface', 
+                        'size', 'count', 'latency', 'duration', 'throughput', 'lost'))
+    #Round throughput to mulplier of hunderds.
+    #df$throughput = round(df$throughput / 100) * 100
+    df$latency = df$latency / 10^3
+    dfc <- summarySE(df, measurevar="latency", groupvars=c("throughput"))
+    d = data.frame(
+        x=dfc$throughput,
+        y=dfc$latency,
+        sd=dfc$sd
+    )
+    pdf('figures/tput-vs-latency.pdf')
+    op <- par(mar = c(7, 7, 3.5, 1.5) + 0.1)
+    plot(d$x, d$y, type="l", cex.axis = 2.0,
+         ylim=c(0,2.2),
+         ylab="",
+         xlab="" )
+    with (
+        data = d,
+        expr = errbar(x, y, y+sd, y-sd, add=T, pch=0.1, cap=.01)
+    )
+    title(xlab="Learner Throughput (Mbps)",
+          ylab="Latency (ms)", cex.lab = 2.5, line = 4)
+    dev.off()
+    
+    
+    dfc <- summarySE(df, measurevar="lost", groupvars=c("throughput"))
+    d = data.frame(
+        x=dfc$throughput,
+        y=dfc$lost,
+        sd=dfc$sd
+    )
+    pdf('figures/tput-vs-loss.pdf')
+    op <- par(mar = c(7, 9, 3.5, 1.5) + 0.1)
+    plot(d$x, d$y, type="l", cex.axis = 2.0,
+         ylab="",
+         xlab="" )
+    with (
+        data = d,
+        expr = errbar(x, y, y+sd, y-sd, add=T, pch=0.1, cap=.01)
+    )
+    title(xlab="Learner Throughput (Mbps)",
+          ylab="Percentage of Packet\nLost or Re-ordered",
+          cex.lab = 2.5, line = 4)
+    dev.off()
+
 }
 
 dir.create(file.path(".", "figures"), showWarnings = FALSE)
 plot_broadcast()
 plot_max_tput()
 plot_netpaxos_tput()
+plot_netpaxos_latency()
--- data/multi-switches/pcap/plot-latency.R
+++ data/multi-switches/pcap/plot-latency.R
@@ -2,12 +2,14 @@ library('ggplot2')
 library('plyr')
 
 args <- commandArgs(TRUE)
-df <- data.frame(seq=numeric(), cid=numeric(), size=numeric(), interface=character(), count=numeric(),
-                        latency=numeric(), interval=numeric(), throughput=numeric())
+df <- data.frame(seq=numeric(), cid=numeric(), size=numeric(), 
+                    interface=character(), count=numeric(),
+                    latency=numeric(), interval=numeric(), throughput=numeric())
 
 for (i in args) {
 
-    data <- read.csv(file=i, sep=",", col.names=c('seq','cid', 'send_time', 'interface', 'receive_time', 'size'))
+    data <- read.csv(file=i, sep=",", col.names=c('seq','cid', 'send_time', 
+                        'interface', 'receive_time', 'size'))
     #data$time <- strptime(data$receive_time, "%H:%M:%S")
     op <- options(digits.secs=6)
     data$send_time <- strptime(data$send_time, "%H:%M:%OS")
@@ -16,10 +18,14 @@ for (i in args) {
     data$receive_time <- as.POSIXct(data$receive_time)
     data$latency <- as.numeric(data$receive_time - data$send_time) * 10^6
     duration <- as.numeric(tail(data$receive_time,1) - head(data$send_time,1))
-    s <- ddply(data, c('interface', 'size'), summarise, count=length(size), latency=mean(latency))
+
+    s <- ddply(data, c('interface', 'size'), summarise, count=length(size), 
+                latency=mean(latency))
+
     s$latency <- round(s$latency)
     s$duration = round(duration,2)
     s$throughput = round(s$count * (s$size * 8) / 10^6 / duration)
+    print(head(s))
     #df <- rbind(df, s)
     write.table(s, append=T, file="output/latency.csv", sep="," , col.names=F,
            row.names=F, quote=F)
@@ -28,7 +34,9 @@ for (i in args) {
 #dir.create(file.path(".", "output"), showWarnings = FALSE)
 #pdf('output/latency-tput.pdf')
 
-#ggplot(df, aes(x=interval, y=throughput)) + xlab('Time (s)') + ylab('Throughput (Mbps)') +  geom_line() + geom_point()
-#ggplot(df, aes(x=interval, y=latency)) + geom_line() + geom_point() + xlab('Time (s)') + ylab('Latency (ms)')
+#ggplot(df, aes(x=interval, y=throughput)) + xlab('Time (s)') + 
+#ylab('Throughput (Mbps)') +  geom_line() + geom_point()
+#ggplot(df, aes(x=interval, y=latency)) + geom_line() + geom_point() + 
+#xlab('Time (s)') + ylab('Latency (ms)')
 
 #dev.off()
